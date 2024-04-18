a538dfc8889827d84751e899dc6aca7f8e7c5ace
bishalmedhi <bishalmedhi@Bishals-MacBook-Pro.local>
2024-04-19 01:00:42 +0530

    Initial commit

---
+++ python/arithmetic-operator.py
@@ -0,0 +1,32 @@
+# variable = {operand} {operator} {operand} #output:{result}
+
+# addition
+result_1 = 14 + 7 # output: 21
+
+# substraction 
+result_2 = 21 - 5 # output: 16
+
+# multiplication
+result_3 = 5 * 2 # output: 10
+
+# division
+result_4 = 4 / 3 # output: 1.33..
+
+# modulus
+result_5 = 5 % 4 # output: 1
+
+# exponentiation
+result_6 = 3 ** 2 # output: 9
+result_7 = 3 ** 0 # output: 1
+
+# floor division
+result_8 = 5 // 3 # output: 1
+
+print(f"result_1: {result_1}")
+print(f"result_2: {result_2}")
+print(f"result_3: {result_3}")
+print(f"result_4: {result_4}")
+print(f"result_5: {result_5}")
+print(f"result_6: {result_6}")
+print(f"result_7: {result_7}")
+print(f"result_8: {result_8}")
---
+++ python/variable-data-type.py
@@ -0,0 +1,15 @@
+# intenger or int 
+a = 12
+
+# float 
+b = 17.5
+
+# string or str
+c = "google"
+
+# boolean or bool
+d = True
+e = False
+
+print(a,b,c,d,e)
+
